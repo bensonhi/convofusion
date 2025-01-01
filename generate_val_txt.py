b46e289a8fa70d1b67deacb04c08ec6a1b7c2fe5
wangbingsheng <s980245n@gmail.com>
2025-01-02 06:03:16 +0800

    first commit

--- generate_val_txt.py
+++ generate_val_txt.py
@@ -1,26 +1,44 @@
 import os
 import random
 
-def list_folders_in_directory(directory, output_file):
+def list_folders_in_directory(directory, val_file, train_file):
     try:
+        val_folders = []
+        train_folders = []
+
         # Get all folders in the directory
         for p in os.listdir(directory):
             folder = os.path.join(directory, p)
-            folders = [os.path.join(p, file) for file in os.listdir(folder) if ((os.path.isdir(os.path.join(folder, file)))  & (random.random()>0.8))]
+            if os.path.isdir(folder):  # Check if it is a folder
+                for file in os.listdir(folder):
+                    subfolder = os.path.join(folder, file)
+                    if os.path.isdir(subfolder):  # Check if it is a subfolder
+                        if random.random() > 0.8:
+                            val_folders.append(os.path.join(p, file))
+                        else:
+                            train_folders.append(os.path.join(p, file))
 
-        # Write folder names to the output file
-        with open(output_file, 'w') as file:
-            for folder in folders:
+        # Write validation folder names to the val_file
+        with open(val_file, 'w') as file:
+            for folder in val_folders:
                 file.write(folder + '\n')
 
-        print(f"List of folders successfully written to {output_file}")
+        # Write training folder names to the train_file
+        with open(train_file, 'w') as file:
+            for folder in train_folders:
+                file.write(folder + '\n')
+
+        print(f"Validation folders written to {val_file}")
+        print(f"Training folders written to {train_file}")
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
-# Specify the directory and output file
+# Specify the directory and output files
 directory = '/home/bwang/ar85/ben/convofusion/datasets/dnd_processed_5sec/'
-output_file = '/home/bwang/ar85/ben/convofusion/datasets/dnd_processed_5sec/val.txt'
+val_file = '/home/bwang/ar85/ben/convofusion/datasets/dnd_processed_5sec/val.txt'
+train_file = '/home/bwang/ar85/ben/convofusion/datasets/dnd_processed_5sec/train.txt'
 
 # Call the function
-list_folders_in_directory(directory, output_file)
+list_folders_in_directory(directory, val_file, train_file)
