040b67cb36415de2471ee12558b4dfa432179d15
wangbingsheng <s980245n@gmail.com>
2025-01-16 16:53:48 +0800

    first commit

---
+++ dnd_generate_val_txt.py
@@ -0,0 +1,54 @@
+import os
+import random
+
+def list_folders_in_directory(directory, val_file, train_file, test_file):
+    try:
+        val_folders = []
+        train_folders = []
+        test_folders =[]
+
+        # Get all folders in the directory
+        for p in os.listdir(directory):
+            folder = os.path.join(directory, p)
+            if os.path.isdir(folder):  # Check if it is a folder
+                for file in os.listdir(folder):
+                    subfolder = os.path.join(folder, file)
+                    if os.path.isdir(subfolder):  # Check if it is a subfolder
+                        variable = random.random()
+                        if variable > 0.9:
+                            val_folders.append(os.path.join(p, file))
+                        elif random.random() > 0.8:
+                            test_folders.append(os.path.join(p, file))
+                        else:
+                            train_folders.append(os.path.join(p, file))
+
+        # Write validation folder names to the val_file
+        with open(val_file, 'w') as file:
+            for folder in val_folders:
+                file.write(folder + '\n')
+
+        with open(test_file, 'w') as file:
+            for folder in test_folders:
+                file.write(folder + '\n')
+
+        # Write training folder names to the train_file
+        with open(train_file, 'w') as file:
+            for folder in train_folders:
+                file.write(folder + '\n')
+
+        print(f"Validation folders written to {val_file}")
+        print(f"Training folders written to {train_file}")
+        print(f"Training folders written to {test_file}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# Specify the directory and output files
+directory = './datasets/DnD_processed_jointpos/'
+val_file = './datasets/DnD_processed_jointpos/val.txt'
+test_file = './datasets/DnD_processed_jointpos/test.txt'
+train_file = './datasets/DnD_processed_jointpos/train.txt'
+
+# Call the function
+list_folders_in_directory(directory, val_file, train_file, test_file)
