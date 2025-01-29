1708cea3b07189c9bb441e3d17ccb7821561a6a8
wangbingsheng <s980245n@gmail.com>
2025-01-29 22:05:37 +0800

    first commit

--- convofusion/models/modeltype/convofusion.py
+++ convofusion/models/modeltype/convofusion.py
@@ -541,7 +541,7 @@ class Convofusion(BaseModel):
                 noise_pred =  noise_pred_uncond + (noise_pred_text + noise_pred_audio + noise_pred_spk + noise_pred_apb + noise_pred_lsnid + noise_pred_all)
             
             # att_mats = [att_mat.chunk(guidance_bs_mulitplier)[1] for att_mat in att_mats]
-            latents = self.scheduler.step(noise_pred, t, latents,
+            latents = self.scheduler.step(noise_pred, t, latents.cuda(),
                                               **extra_step_kwargs).prev_sample
             
 
