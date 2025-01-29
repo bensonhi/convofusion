efed25c117c954fcec55752ab43b8cf17f43ce08
wangbingsheng <s980245n@gmail.com>
2025-01-30 00:44:13 +0800

    first commit

--- convofusion/models/modeltype/convofusion.py
+++ convofusion/models/modeltype/convofusion.py
@@ -541,6 +541,10 @@ class Convofusion(BaseModel):
                 
                 noise_pred =  noise_pred_uncond + (noise_pred_text + noise_pred_audio + noise_pred_spk + noise_pred_apb + noise_pred_lsnid + noise_pred_all)
 
+
+            print(self.device)
+            print(latents.device)
+            print(hihi)
             t=t.to(latents.device)
             latents=latents.to(latents.device)
             noise_pred = noise_pred.to(latents.device)
