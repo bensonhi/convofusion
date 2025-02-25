772f2de1c393b10c07e13edb59392316d37f188d
Benson <s980245n@gmail.com>
2025-02-25 15:56:20 +0800

    fix the size of time_emb

--- convofusion/models/architectures/denoiser.py
+++ convofusion/models/architectures/denoiser.py
@@ -271,10 +271,7 @@ class Denoiser(nn.Module):
                 tlsn = time_emb + tlsn
                 # aspk = time_emb + aspk
                 audio_time_emb = self.time_to_audio_proj(time_emb)
-                print(time_emb.shape)
-                print(audio_time_emb.shape)
-                print(alsn.shape)
-                print(spk_emb.shape)
+                audio_time_emb=audio_time_emb[:,alsn.shape[1]:]
                 alsn = audio_time_emb + alsn
                 spk_emb = audio_time_emb + spk_emb
                 apb = time_emb + apb
